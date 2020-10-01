c0d4c5c582a976ff0f6d12518b11291399697da2
alexanders0 <alexandersn059@gmail.com>
2020-09-30 22:35:51 -0500

    administration dashboard customization

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
+""" User admin classes """
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from users.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """ Profile admin """
+    # Mostrar datos de cada usuario al listar la app en el Admin
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    # Establecer datos como links
+    list_display_links = ('pk', 'user')
+    # Permitir editar datos sin entrar al registro
+    list_editable = ('phone_number', 'website', 'picture')
+    # Buscar por campos
+    search_fields = (
+        'user__email',
+        'user__username',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+    # Agregar filtros (el orden importa)
+    list_filter = (
+        'created',
+        'modified',
+        'user__is_active',
+        'user__is_staff'
+    )
