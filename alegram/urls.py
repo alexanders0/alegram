9f087c309efd056ea782135c94b1f1ab0b3ff160
alexanders0 <alexandersn059@gmail.com>
2020-10-03 00:08:55 -0500

    posts models created and posts admin dashboard configuration

--- alegram/urls.py
+++ alegram/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from alegram import views as local_views
@@ -26,4 +28,4 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>/', local_views.say_hi),
 
     path('posts/', posts_views.list_posts)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,29 @@
+""" Post admin classes """
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """ Post admin. """
+
+    list_display = ('pk', 'title', 'photo', 'user')
+    list_display_links = ('pk', 'title')
+    list_editable = ('photo',)
+    search_fields = ('user__username', 'title')
+    list_filter = ('created', 'modified')
+    fieldsets = (
+        ('Author', {
+            'fields': (('user', 'profile'),)
+        }),
+        ('Details', {
+            'fields': (
+                ('title', 'photo')
+            )
+        })
+    )
+    readonly_fields = ('created', 'modified')
