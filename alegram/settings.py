39d074a21abe747dbd5e7d403a5fa87051886582
alexanders0 <alexandersn059@gmail.com>
2021-02-12 14:48:14 -0500

    Creation of middleware to ensure every user have their profile picture and biography

--- alegram/settings.py
+++ alegram/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'alegram.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'alegram.urls'
