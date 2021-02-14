e7747f7a5dcdcb0982238dd265964f348cbc2fc9
alexanders0 <alexandersn059@gmail.com>
2021-02-14 11:01:15 -0500

    Detail View and List View implementation for user profile and posts list, respectively

--- posts/views.py
+++ posts/views.py
@@ -3,6 +3,8 @@
 # Django
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import ListView
 
 # Forms
 from posts.forms import PostForm
@@ -11,11 +13,14 @@ from posts.forms import PostForm
 from posts.models import Post
 
 
-@login_required
-def list_posts(request):
-    """ List existing posts """
-    posts = Post.objects.all().order_by('-created')
-    return render(request, 'posts/feed.html', {'posts': posts})
+class PostsFeedView(LoginRequiredMixin, ListView):
+    """ Return all published posts """
+
+    template_name = 'posts/feed.html'
+    model = Post
+    ordering = ('-created')
+    paginate_by = 2
+    context_object_name = 'posts'
 
 
 @login_required
--- users/views.py
+++ users/views.py
@@ -3,11 +3,34 @@
 # Django
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib import messages
+from django.views.generic.detail import DetailView
+
+# Models
+from django.contrib.auth.models import User
+from posts.models import Post
 
 # Forms
 from users.forms import ProfileForm, SignupForm
+
+
+class UserDetailView(LoginRequiredMixin, DetailView):
+    """ User detail view """
+    template_name = 'users/detail.html'
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    queryset = User.objects.all()
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        """ Add user's posts to context """
+        context = super().get_context_data(**kwargs)
+        user = self.get_object()
+        context["posts"] = Post.objects.filter(user=user).order_by('-created')
+        return context
 
 
 @login_required
@@ -28,7 +51,8 @@ def update_profile(request):
             profile.save()
             messages.success(request, 'Profile successfully updated')
 
-            return redirect('users:update_profile')
+            url = reverse('users:detail', kwargs={'username': request.user.username})
+            return redirect(url)
     else:
         form = ProfileForm()
 
